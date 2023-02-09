8868b097f7f0f830b109b4ad12316a6b281ba8ce
Kaiy <kkm4289@rit.edu>
2023-02-08 21:05:05 -0500

    tweaking to fit ACVs. Error with multivariate norm

--- src/ml_models/linearTS.py
+++ src/ml_models/linearTS.py
@@ -4,19 +4,30 @@ class LinearThompsonSampling:
     def __init__(self, d):
         self.d = d
         # Covariance matrix, initialized as identity matrix
-        self.A = np.identity(d)
-        self.b = np.zeros(d)  # Observation vector, initialized as zero vector
+        self.A = [np.identity(d)] * 4
+        self.b = [np.zeros((d, 1))] * 4  # Observation vector, initialized as zero vector
+        self.theta = [np.zeros((d, 1))] * 4
 
-    def update(self, x, reward):
-        x = x.reshape(-1, self.d)
-        self.A += np.dot(x.T, x)  # Update covariance matrix
-        self.b += reward * x.reshape(-1)  # Update observation vector
+    def update(self, arm, x, reward):
+        x = np.array(x).reshape(-1, 1)
+        self.A[arm] += np.dot(x, x.T)  # Update covariance matrix
+        self.b[arm] += reward * x.reshape(-1)  # Update observation vector
+        self.theta[arm] = np.linalg.inv(self.A[arm]).dot(self.b[arm])
 
-    def choose_arm(self, x):
-        x = x.reshape(-1, self.d)
-        theta_samples = np.random.multivariate_normal(
-            np.linalg.inv(self.A).dot(self.b), np.linalg.inv(self.A), size=1)
+
+    def select_arm(self, readings):
+        # x = x.reshape(-1, self.d)
+        length = len(readings)
+        ts = [0] * length
+        for i in range(length):
+            x = np.array(readings[i]).reshape(-1, self.d)
+            print(np.linalg.inv(self.A[i]).dot(
+                self.b[i]))
+            print(np.linalg.inv(self.A[i]))
+
+            ts[i] = np.random.multivariate_normal(
+                np.linalg.inv(self.A[i]).dot(self.b[i]), np.linalg.inv(self.A[i]), size=1)
         # Calculate the mean of the estimated rewards for each arm
-        means = x.dot(theta_samples.T)
+            means = x.dot(ts[i].T)
         # Choose the arm with the maximum estimated reward
         return np.argmax(means)
