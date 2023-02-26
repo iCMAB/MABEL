922c2a8ef4e418d87a637dc4d353a7b09f07900a
varsha-venk <100494832+varsha-venk@users.noreply.github.com>
2023-02-26 16:42:46 -0500

    comments added

--- src/ml_models/bernoulliMAB.py
+++ src/ml_models/bernoulliMAB.py
@@ -4,10 +4,13 @@ import random
 class BernoulliEpsilon:
     # theta - penalty calculator
     def __init__(self, d, epsilon):
+        # Epsilon is the probability with which an arm is selected.
         self.epsilon = epsilon
         self.d  = d
+        # Intially all arms have the same penalties.
         self.theta = [np.identity(d)] * 4
 
+    # Selection of the arm happens using epsilon-greedy strategy
     def select_arm(self):
         myRandVal = random.uniform(0,1)
         if myRandVal < self.epsilon:
@@ -19,5 +22,8 @@ class BernoulliEpsilon:
             # Exploit: Choose the arm with the highest estimated value
             return np.argmax(self.theta)
 
+    #Updating of values happens using penalty values
+    # method takes as input the index of the arm that was played and the observed penalty, 
+    # and updates the estimated value of that arm using the formula for a sample mean.
     def update(self, arm, readingsVal, penalty):
         self.theta[arm] += (penalty - self.theta[arm]) / (penalty + 1)
