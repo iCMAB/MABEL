c1a38dbc494ff66728c030f137d8f6d0d9af5327
Kaiy <kkm4289@rit.edu>
2023-02-27 20:47:16 -0500

    ucb1 init

--- src/ml_models/UCB1.py
+++ src/ml_models/UCB1.py
@@ -38,23 +38,3 @@ class UCB1:
             arm = np.argmax(ucb_values)
         self.total_selections += 1 
         return arm
-
-
-    # def __init__(self, d, epsilon):
-    #     self.epsilon = epsilon
-    #     self.d = d
-    #     self.values = [np.identity(d)] * 4
-
-    # def select_arm(self):
-    #     myRandVal = random.uniform(0, 1)
-    #     if myRandVal < self.epsilon:
-    #         print("i'm exploring")
-    #         # Explore: Choose a random arm with probability epsilon
-    #         return np.random.randint(0, len(self.values)-1)
-    #     else:
-    #         print("i'm exploiting")
-    #         # Exploit: Choose the arm with the highest estimated value
-    #         return np.argmax(self.values)
-
-    # def update(self, arm, reward):
-    #     self.values[arm] += (reward - self.values[arm]) / (reward + 1)
