e759d0410efaf5512bf112be05cb316dbb79bc7a
Kaiy <kkm4289@rit.edu>
2023-02-28 01:08:26 -0500

    UCB1 penalty

--- src/ml_models/UCB1.py
+++ src/ml_models/UCB1.py
@@ -1,23 +1,16 @@
 import numpy as np
+from ml_models.MABModel import MABModel
 
-class UCB1:
-    def __init__(self, d, n_arms, alpha):
-        self.n_arms = n_arms
-        self.d = d
+"""Seeks to minimize penalty"""
+class UCB1_Normal_Penalized(MABModel):
+    def __init__(self, **kwargs):
+        self.n_arms = kwargs.get('n_arms')
+        self.d = kwargs.get('d')
         self.total_selections = 0
-        self.total_rewards = np.zeros(n_arms)
-        self.num_selections = np.zeros(n_arms)
-        self.arm_features = np.zeros((n_arms, d))
+        self.theta = [np.identity(self.d)] * self.n_arms
+        self.num_selections = [np.identity(self.d)] * self.n_arms
 
-    def update(self, arm, reward, x):
-        """
-        Update the statistics for arm with observed reward and feature vector `x`.
-        """
-        self.total_rewards[arm] = self.total_rewards[arm] + reward
-        self.num_selections[arm] = self.num_selections[arm] + 1
-        self.arm_features[arm] = x
-
-    def select_arm(self):
+    def select_arm(self, **kwargs):
         """
         Choose the arm with the highest UCB val based on the current estimates of the mean reward and variance.
         """
@@ -25,9 +18,19 @@ class UCB1:
         if self.total_selections < self.n_arms:
             arm = self.total_selections
         else:
-            #  Then calculate UCB value for each arm and choose the arm with the highest value
-            ucb_values = self.total_rewards / self.num_selections + \
-                np.sqrt(2 * np.log(self.total_selections) / self.num_selections)
-            arm = np.argmax(ucb_values)
-        self.total_selections += 1 
+            #  Then calculate UCB value for each arm and choose the arm with the smallest penalty
+            ucb_values = []
+            for i in range(len(self.theta)):
+                ucb_values.append(self.theta[i] / self.num_selections[i] - np.sqrt(2 * np.log(self.total_selections) / self.num_selections[i]))
+            arm = np.argmin(ucb_values)
+        self.total_selections += 1
         return arm
+
+    def update(self, **kwargs):
+        """
+        Update the statistics for arm with observed reward and feature vector `x`.
+        """
+        arm = kwargs.get('arm')
+        penalty = kwargs.get('penalty')
+        self.theta[arm] = self.theta[arm] + penalty
+        self.num_selections[arm] = self.num_selections[arm] + 1
