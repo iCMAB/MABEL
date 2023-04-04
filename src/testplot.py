1d9f1f7691e5fc3a82f602604767667c65adaa7f
Alex Hetrick <aph1777@rit.edu>
2023-04-03 22:12:40 -0400

    Start of simulation visualization using plotly dash

--- src/testplot.py
+++ src/testplot.py
@@ -1,22 +1,78 @@
-import matplotlib.pyplot as plt
+from dash import dash, dcc, html
+from dash.dependencies import Input, Output
+import plotly.graph_objs as go
+import numpy as np
+import os, webbrowser, time
+from threading import Timer
 
-# Create some sample data
-positions = [10, 8, 6, 4]
-distances = [2, 2, 2, 2]
+app = dash.Dash(__name__)
 
-# Plot the positions as circles
-for i in range(len(positions)):
-    plt.scatter(positions, [0]*4, s=100)
-    plt.text(positions[i], -0.1, f"ACV{i}: {positions[i]}", ha='center', va='top')
+x = [i for i in range(5)]
+y = [4-i for i in range(5)]
+interval = 1000
 
-# Add arrows to represent the distances
-for i in range(0, len(positions) - 1):
-    plt.arrow(positions[i+1], 0, distances[i+1], 0)
-    plt.text((positions[i+1]+positions[i])/2, 0.1, f"{distances[i+1]}", ha='center', va='bottom')
+trace = go.Scatter(x=x, y=y, mode='markers', marker=dict(size=15, color='LightSkyBlue'), name="acvs")
 
-# Set the x-axis limits
-plt.xlim(0, 12)
-plt.ylim(-1,1)
+layout = go.Layout(
+    title='ACV Simulation',
+    xaxis=dict(
+        title='Position', 
+        tickmode = 'linear',
+        dtick = 0.5
+    ),
+    yaxis=dict(
+        title='ACVs',
+        type='category',
+        categoryorder='total descending',
+        tickmode='array',
+        tickvals=[0, 1, 2, 3, 4],
+        ticktext=['ACV 0', 'ACV 1', 'ACV 2', 'ACV 3', 'ACV 4']
+    ),
+    height=500,
+    transition={
+        'duration': interval / 2.5,
+        'easing': 'cubic-in-out'
+    }
+)
 
-# Show the plot
-plt.show()
+fig = go.Figure(data=[trace], layout=layout)
+
+app.layout = html.Div([
+    dcc.Graph(id='graph', figure=fig),
+    dcc.Interval(
+        id='data-update',
+        interval=interval,  # in milliseconds
+        n_intervals=0
+    ),
+    dcc.Interval(
+        id='range-update',
+        interval=interval,  # in milliseconds
+        n_intervals=0
+    )
+])
+
+@app.callback(Output('graph', 'figure', allow_duplicate=True), [Input('data-update', 'n_intervals')], prevent_initial_call=True)
+def update_data(n):
+    x = np.copy(fig['data'][0]['x'])
+    x = np.add(x, np.ones(5))
+
+    fig['data'][0]['x'] = x
+
+    return fig
+
+@app.callback(Output('graph', 'figure'), [Input('range-update', 'n_intervals')])
+def update_range(n):
+    time.sleep(0.1)
+    x = np.copy(fig['data'][0]['x'])
+    
+    fig['layout']['xaxis']['range'] = (x[0]-3, x[len(x)-1]+3)
+
+    return fig
+
+def open_browser():
+    if not os.environ.get("WERKZEUG_RUN_MAIN"):
+        webbrowser.open_new('http://127.0.0.1:8050/')
+
+if __name__ == '__main__':
+    Timer(1, open_browser).start()
+    app.run_server(debug=True)
