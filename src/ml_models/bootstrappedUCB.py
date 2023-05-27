557b532c7d0fb0d3f8950b5768b2cee6a7173169
Kaiy <kkm4289@rit.edu>
2023-05-26 20:06:22 -0400

    missed a line

--- src/ml_models/bootstrappedUCB.py
+++ src/ml_models/bootstrappedUCB.py
@@ -1,6 +1,7 @@
 import numpy as np
 from ml_models.MABModel import MABModel
 
+
 class BootstrappedUCB(MABModel):
     def __init__(self, **kwargs):
         self.n_arms = kwargs.get('n_arms')
@@ -30,30 +31,29 @@ class BootstrappedUCB(MABModel):
                 x = np.array(variations[i]).reshape(-1, 1)
                 upper_confidence_bounds[i] = np.dot(self.means[i].T, x) + \
                     np.sqrt(self.alpha * np.log(self.t + 1) / self.n_pulls[i])
-            
+
                 bootstrap_upper_confidence_bounds[i] = np.percentile(
-                    self.bootstrap_means[i], q = 100 * (1-1/(self.t+1)))
-                
+                    self.bootstrap_means[i], q=100 * (1-1/(self.t+1)))
+
                 upper_confidence_bounds[i] += bootstrap_upper_confidence_bounds[i]
-            
+
             arm = np.argmax(upper_confidence_bounds)
 
-        
         self.t += 1
         return arm
 
     def update(self, **kwargs):
         arm = kwargs.get('arm')
         penaltyVal = kwargs.get('penalty')
-        
+
         self.penaltyVals[arm].append(penaltyVal)
         self.n_pulls[arm] += 1
         self.means[arm] = np.mean(self.penaltyVals[arm])
-        
-        bootstrap_indices = np.random.randint(low=0, high=len(self.penaltyVals[arm]), size=(self.n_bootstrap,))
+
+        bootstrap_indices = np.random.randint(low=0, high=len(
+            self.penaltyVals[arm]), size=(self.n_bootstrap,))
         bootstrap_samples = np.array(self.penaltyVals[arm])[bootstrap_indices]
         bootstrap_means = np.mean(bootstrap_samples)
         self.bootstrap_means[arm, :] = bootstrap_means
-        
-        self.theta[arm] = self.means[arm]
 
+        self.theta[arm] = self.means[arm]
