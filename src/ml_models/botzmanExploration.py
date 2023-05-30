cc32444f1bd30814f8b1849b862b94ce4eb925df
varsha-venk <100494832+varsha-venk@users.noreply.github.com>
2023-05-29 17:52:18 -0700

    commit botzman

---
+++ src/ml_models/botzmanExploration.py
@@ -0,0 +1,28 @@
+import numpy as np
+from ml_models.MABModel import MABModel
+
+class BoltzmanExploration(MABModel):
+    def __init__(self, **kwargs):
+        self.n_arms = kwargs.get('n_arms')
+        self.temperature = kwargs.get('temperature')
+        self.alpha = kwargs.get('alpha')
+
+        self.reset()
+
+    def reset(self):   
+        self.q_estimates = np.zeros(self.n_arms)
+        self.action_count = np.zeros(self.n_arms)
+
+    def select_arm(self, **kwargs):
+        boltzmann_probs = np.exp(self.q_estimates / self.temperature) / np.sum(np.exp(self.q_estimates / self.temperature))
+        arm = np.random.choice(np.arange(self.n_arms), p=boltzmann_probs)
+        return arm
+
+    def update(self, **kwargs):
+        arm = kwargs.get('arm')
+        x = kwargs.get('x')
+        penalty = kwargs.get('penalty')
+        self.action_count[arm] += 1
+        alpha = 1 / self.action_count[arm]
+        self.q_estimates[arm] = (1 - alpha) * self.q_estimates[arm] + alpha * (penalty)
+
